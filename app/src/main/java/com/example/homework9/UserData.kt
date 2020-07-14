e6d184dc09298512bc490ea817c14d5bd15767ac
Aiperi <aip.amankulova@gmail.com>
2020-07-14 18:58:50 +0600

    Adding UserData to our application

---
+++ app/src/main/java/com/example/homework9/UserData.kt
@@ -0,0 +1,18 @@
+package com.example.homework9
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UserData(
+    val tauranga: String,
+    val lakhimpur: String,
+    val unnao: String,
+    var saga: String,
+    val pikine: Int,
+    val naney: String,
+    var changping: String,
+    var lubao: String,
+    var bratsk: String,
+    var tulock: String
+) : Parcelable
